a77f695b782b3f1b4e3b75fd093d139a00eb4071
Stanislav Sinyagin <ssinyagin@k-open.com>
2022-03-19 20:11:06 +0000

    it works

---
+++ README.md
@@ -0,0 +1,9 @@
+This is an adaptation of
+https://github.com/pcbreflux/espressif/tree/master/esp32/app/ESP32_micro-ecc
+
+to the current ESP-IDF (git master branch, commit 1d77b5b6 of Mon Feb 14 17:13:55 2022 +0000)
+
+
+
+ssinyagin@gmail.com
+
---
+++ main/main.c
@@ -0,0 +1,35 @@
+/* Copyright (c) 2017 pcbreflux. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>. *
+ */
+#include <string.h>
+#include <stdlib.h>
+
+#include "sdkconfig.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "esp_log.h"
+#include "esp_system.h"
+
+#include "ecc_task.h"
+
+#define TAG "MAIN"
+
+void app_main() {
+
+    xTaskCreate(ecc_task, "gpio_task", 12288, NULL, 5, NULL);
+
+}
+
